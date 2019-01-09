b8779413f17ac60cf422d065cf9b8f96182012e2
zkedem <zkedem@student.mamkschools.org>
2019-01-09 14:48:31 -0500

    This is a test.

--- src/Ch7Arrays/PersonalityTest.java
+++ src/Ch7Arrays/PersonalityTest.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class PersonalityTest {
 
+    // This is a test.
     public static void main(String[] args) throws FileNotFoundException {
         Scanner console = new Scanner(System.in);
         System.out.print("Input file name: ");
